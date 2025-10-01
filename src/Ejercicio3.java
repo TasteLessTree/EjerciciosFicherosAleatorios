0a175caa45453c3d7b8c36eec89d0e9280cf640f
Andrés <spelujo@gmail.com>
2025-10-01 15:46:45 +0200

    Intento ejercicio 3

---
+++ src/Ejercicio3.java
@@ -0,0 +1,99 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+/** @author AndrésPérezM
+ * */
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        File fichero = new File("data/juegos.dat");
+
+        /*if (fichero.delete()) {
+            System.out.println("Fichero eliminado");
+        }*/
+
+        System.out.println("¿Cuál es el identificador del juego?");
+        System.out.print(">> ");
+        int id = sc.nextInt();
+
+        sc.nextLine();
+        System.out.println("¿Cuál es el nombre del juego?");
+        System.out.print(">> ");
+        String nombre = sc.nextLine();
+
+        System.out.println("¿Cuál es la edad mínima para jugar el juego?");
+        System.out.print(">> ");
+        int edad = sc.nextInt();
+
+        sc.nextLine();
+        System.out.println("¿Cuál es la clasificación del juego?");
+        System.out.print(">> ");
+        String clasificacion = sc.nextLine();
+
+        anyadirJuego(fichero, id, nombre, edad, clasificacion);
+        mostrarNumeros(fichero);
+    }
+
+    public static void anyadirJuego(File fichero, int id, String nombre, int edad, String clasificacion) {
+        try (RandomAccessFile file = new RandomAccessFile(fichero, "rw")) {
+            long posicion = file.length();
+            file.seek(posicion);
+
+            file.writeInt(id);
+
+            StringBuilder titulo = new StringBuilder(nombre);
+            titulo.setLength(10);
+            file.writeChars(titulo.toString());
+
+            file.writeInt(edad);
+
+            StringBuilder clasif = new StringBuilder(clasificacion);
+            clasif.setLength(10);
+            file.writeChars(clasif.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void mostrarNumeros(File fichero) {
+        System.out.println("===== [" + fichero.getName() + "] =====");
+
+        long posicion = 0;
+
+        int id;
+        char[] nombre = new char[10];
+        int edad;
+        char[] clasificacion = new char[10];
+
+        try (RandomAccessFile file = new RandomAccessFile(fichero, "r")) {
+            while (file.getFilePointer() < file.length()) {
+                file.seek(posicion);
+                id = file.readInt();
+
+                for (int i = 0; i < nombre.length; i++) {
+                    nombre[i] = file.readChar();
+                }
+
+                edad = file.readInt();
+
+                for (int i = 0; i < clasificacion.length; i++) {
+                    clasificacion[i] = file.readChar();
+                }
+
+                String titulo = new String(nombre);
+                String clasif = new String(clasificacion);
+
+                System.out.println("Número de orden: " + id + ". Título: " + titulo + ". Edad mínima: " + edad + ". Claficiación: " + clasif);
+                posicion += 28;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("\n===== [" + fichero.getName() + "] =====");
+        System.out.println();
+    }
+}
